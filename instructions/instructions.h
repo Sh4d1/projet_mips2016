387e931bf9edb1af0ec4053b53d718338815e4fd
Patrik Cyvoct <patrik.cyvoct@gmail.com>
2016-05-30 14:42:36 +0200

    Ajout codes instructions

---
+++ instructions/instructions.h
@@ -0,0 +1,40 @@
+#ifndef _INSTRUCTIONS_
+#define _INSTRUCTIONS_
+
+enum function = {
+    ADD = 32,
+    ADDU = 33,
+    AND = 36,
+    DIV = 26,
+    JR = 8,
+    MFHI = 16,
+    MFLO = 18,
+    MULT = 24,
+    OR = 37,
+    SLL = 0,
+    SRL = 2,
+    SLT = 42,
+    SUB = 34,
+    SYSCALL = 12,
+    XOR = 38
+};
+
+enum opcode = {
+    ADDI = 16,
+    ADDIU = 17,
+    BEQ = 4,
+    BGTZ = 7,
+    BLEZ = 6,
+    BNE = 5,
+    J = 2,
+    JAL = 3,
+    LB = 32,
+    LBU = 36,
+    LUI = 15,
+    LW = 35,
+    ORI = 13,
+    SB = 40,
+    SW = 43
+};
+
+#endif /* end of include guard: _INSTRUCTIONS_ */

d21f2830ca5533a13ac59d56415ec0802ff374dc
Quentin Lapointe <qlapoint@grenoble-inp.ensimag.fr>
2016-06-01 18:56:10 +0200

    get_string

--- src/mem.c
+++ src/mem.c
@@ -99,7 +99,7 @@ uint32_t get_memory_size()
 char* get_string(uint32_t address) {
     uint32_t string_length = 0;
     while (get_byte(address + string_length++));
-    string = malloc(string_length * sizeof(char));
+    char *string = malloc(string_length * sizeof(char));
     for (uint32_t i = 0; i < string_length; i++) {
         *string[i] = get_byte(address + i);
     }

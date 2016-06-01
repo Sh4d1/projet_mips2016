5f12a34d86f967a3430cba47426c46b75ecb5753
Quentin Lapointe <qlapoint@grenoble-inp.ensimag.fr>
2016-06-01 16:50:08 +0200

    erreur sw

--- proc.c
+++ proc.c
@@ -12,7 +12,7 @@ int main()
         init_GPR();
         init_memory(0x1000);
         set_register_value(10, 10);
-        sw(10, 0x2000000);
+        sw(10, 0x2000000, 0);
 
         return 0;
 }

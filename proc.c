8c4ef43e3e3704c36b92df85f9e5e9322b3e962d
Quentin Lapointe <qlapoint@grenoble-inp.ensimag.fr>
2016-06-01 14:44:42 +0200

    test

--- proc.c
+++ proc.c
@@ -4,14 +4,18 @@
 #include "include/gpr.h"
 #include "include/mem.h"
 
+#define INT32_MAX 2147483647
+#define UINT32_MAX 4294967295
+
 int main()
 {
         init_GPR();
         init_memory(0x1000);
-        set_register_value(5, 99);
-        printf("%d\n", get_register_value(5));
-        sw(5, 0xE00, 0);
-        lw(6, 0xE00, 0);
-        printf("%d\n", get_register_value(6));
+        set_register_value(10, INT32_MAX);
+        set_register_value(11, 2);
+        mult(10, 11);
+        printf("%d\n", get_LO_value());
+        printf("%d\n", get_HI_value());
+
         return 0;
 }

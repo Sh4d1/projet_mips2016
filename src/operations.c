325f7c6d3d1e3c973314e69442ce046489e943df
Quentin Lapointe <qlapoint@grenoble-inp.ensimag.fr>
2016-06-01 15:14:16 +0200

    test

--- src/operations.c
+++ src/operations.c
@@ -75,7 +75,7 @@ void mult(uint8_t rs, uint8_t rt)
         uint64_t x = get_register_value(rs);
         uint64_t y = get_register_value(rt);
         int64_t res = x * y;
-        printf("%llx\n", res);
+        printf("%llx\n", 0xFFFFFFFFFFFFFFFF);
         set_LO_value(res);
         set_HI_value(res >> 32);
 }

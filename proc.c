f3494e1fc510ac6246a9d365225872e62d921303
Quentin Lapointe <qlapoint@grenoble-inp.ensimag.fr>
2016-06-01 14:39:23 +0200

    test

--- proc.c
+++ proc.c
@@ -8,6 +8,10 @@ int main()
 {
         init_GPR();
         init_memory(0x1000);
-        printf("%x\n", get_register_value(29));
+        set_register_value(5, 99);
+        printf("%d\n", get_register_value(5));
+        sw(5, 0xE00, 0);
+        lw(6, 0xE00, 0);
+        printf("%d\n", get_register_value(6));
         return 0;
 }

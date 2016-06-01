ed8de7ad97dbc7fe273cbdd75f2553a10a4f37ba
Quentin Lapointe <qlapoint@grenoble-inp.ensimag.fr>
2016-06-01 19:46:31 +0200

    test

--- proc.c
+++ proc.c
@@ -9,9 +9,10 @@ int main()
     init_GPR();
     init_memory(0x1000000);
     set_string(0x1000, "bonjour");
-    set_register_value_by_name("v0", 4);
+    set_register_value_by_name("v0", 5);
     set_register_value_by_name("a0", 0x1000);
     syscall();
+    printf("%u", get_register_value("v0"));
     free_memory();
     return 0;
 }

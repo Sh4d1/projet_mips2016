3005c9253f16d2aaf023fd71b6e039344aa92e20
Quentin Lapointe <qlapoint@grenoble-inp.ensimag.fr>
2016-06-02 17:03:47 +0200

    test mehdi

---
+++ test_framebuffer.c
@@ -0,0 +1,13 @@
+#include "include/framebuffer.h"
+
+int main() {
+    uint8_t *premier_pixel = framebuffer_init_display();
+    int32_t index = 0;
+    while (index > 0) {
+        printf("Ecire un numero de pixel entre 0 et (320 * 200) - 1 :\nNégatif pour terminer.\n");
+        scanf("%d", &index);
+        (*premier_pixel)[index] = 0xFF; //noir
+    }
+    framebuffer_close_display();
+    return 0;
+}

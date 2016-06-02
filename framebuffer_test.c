9133b89571ef64f62d0b2d4f4a9784c259b0350c
Quentin Lapointe <qlapoint@grenoble-inp.ensimag.fr>
2016-06-02 17:19:37 +0200

    test mehdi

--- framebuffer_test.c
+++ framebuffer_test.c
@@ -6,7 +6,7 @@
 int main() {
     uint8_t *premier_pixel = framebuffer_init_display();
     int32_t index = 0;
-    while (index > 0) {
+    while (index >= 0) {
         printf("Ecire un numero de pixel entre 0 et (320 * 200) - 1 :\nNégatif pour terminer.\n");
         scanf("%d", &index);
         premier_pixel[index] = 0xFF; //noir

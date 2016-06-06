5654b6dda48b6fa9f0c6bfc227c395ae59731650
Patrik Cyvoct <patrik.cyvoct@gmail.com>
2016-06-06 17:05:50 +0200

    added fb test

---
+++ test/fb.s
@@ -0,0 +1,24 @@
+.global _start
+
+.set noreorder
+.text
+_start:
+MOVE $t0, $0
+MOVE $t1, $0
+ADDI $t3, 320
+ADDI $t4, 200
+LUI $t2, 0xFFFF
+ADDI $t2, $t2, 0x0600
+while:
+BEQ $t0, $t4, endwhile
+MOVE $t1, $0
+while2:
+BEQ $t1, $t3, endwhile2
+SB $t0, 0($t2)
+ADDI $t1, $t1, 1
+ADDI $t2, $t2, 1
+J while2
+endwhile2:
+ADDI $t0, $t0, 1
+J while
+endwhile:
